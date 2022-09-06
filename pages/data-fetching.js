48a031a6341a478c848e7c99005121da2136ec31
pkovalchuk <pavel.kovalchuk@dataart.com>
2022-09-06 12:10:16 +0200

    Incremental static generation

--- pages/data-fetching.js
+++ pages/data-fetching.js
@@ -14,16 +14,37 @@ function HomePage(props) {
 }
 
 // first step of prerendering
-export async function getStaticProps() {
+export async function getStaticProps(context) {
   console.log('(Re-)Generating...');
   const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
 
+  if (!data) {
+    return {
+      redirect: {
+        destination: '/no-data',
+      },
+    };
+  }
+
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
+
   return {
     props: {
       products: data.products,
     },
+    // revalidate is for Incremental static generation (update data on each request after the build)
+    // for production only
+    revalidate: 10,
+    // page will return 404 error if true
+    notFound: false,
+    // for redirects
+    /* redirect: {
+      destination: '/',
+    }, */
   };
 }
 
